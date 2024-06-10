51804d4273a400f2fdd06e093fb59c9f520db0b0
不爱打代码的程序员 <2284609302@qq.com>
2024-06-10 14:37:27 +0800

    修改参数

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -16,7 +16,7 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '学习少儿编程',
-  HEO_HERO_TITLE_2: '就到coding Lab !',
+  HEO_HERO_TITLE_2: '就到coding Lab ',
   HEO_HERO_TITLE_3: 'codinglab.online',
   HEO_HERO_TITLE_4: '收藏网址，关注更多内容',
   HEO_HERO_TITLE_5: 'coding Lab 少儿编程实验室',
@@ -29,7 +29,7 @@ const CONFIG = {
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   // HERO_RECOMMEND_COVER: '/public/images/heo/scratch.webp', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
